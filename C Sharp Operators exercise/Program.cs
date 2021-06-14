a4981160364083a84510174762004362b14881e1
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-13 23:22:16 -0700

    initial commit

---
+++ C Sharp Operators exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Sharp_Operators_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int a = 17;
+         int b = 4;
+         int quotient = a / b;
+         int remainder = a % b;
+         Console.WriteLine($"{a}/{b} is the {quotient} remainder {remainder}");
+        
+
+
+
+        }
+    }
+}
